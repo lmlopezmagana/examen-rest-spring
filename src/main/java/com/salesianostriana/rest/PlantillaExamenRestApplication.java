2ed75c1eec1e800c3159a6ebeaa8f4339fe4b3ec
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2018-11-14 18:12:51 +0100

    Second commit

---
+++ src/main/java/com/salesianostriana/rest/PlantillaExamenRestApplication.java
@@ -0,0 +1,12 @@
+package com.salesianostriana.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantillaExamenRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantillaExamenRestApplication.class, args);
+	}
+}
